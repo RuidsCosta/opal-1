517d09de5b4aeacb39948332c48abbe27f004272
PedroFerr101 <pferreira59@gmail.com>
2019-09-07 18:00:32 +0100

    Add login logic
    Add sign up HTTP resquest and response handlers;

--- pages/login.js
+++ pages/login.js
@@ -1,19 +1,9 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
+import React,  {useState} from 'react';
+import { Avatar, Button, CssBaseline, TextField, FormControlLabel, Checkbox, Link, Grid, Box, Typography, Container }  from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
 import Footer from '../components/Footer';
-
+import axios from "axios";
 
 const useStyles = makeStyles(theme => ({
   '@global': {
@@ -44,6 +34,40 @@ const useStyles = makeStyles(theme => ({
 const LogIn = () => {
   const classes = useStyles();
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMsg, setErrorMsg] = useState('');
+
+
+  const login = (email, password) => {
+    const api_endpoint = "https://safira20.herokuapp.com/api/auth/sign_in/";
+    
+    axios.post(api_endpoint, {
+      email: email,
+      password: password
+    })
+    .then(res => handleLoginResponse(res))
+    .catch(res => handleErrorResponse(res));
+  } 
+  
+  const handleLoginResponse = (res) => {
+    if (res.data.hasOwnProperty("jwt")) {
+      setErrorMsg('');
+      localStorage.jwt = res.data.jwt;
+      window.location.pathname = "/";
+    } else {
+      setErrorMsg('Login error');
+    }
+  }
+  
+  const handleErrorResponse = (res) => {
+    if (res.response) {
+      setErrorMsg('Invalid email or password');
+    } else {
+      setErrorMsg('Login error');
+    }
+  }
+
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
@@ -54,7 +78,6 @@ const LogIn = () => {
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
-        <form className={classes.form} noValidate>
           <TextField
             variant="outlined"
             margin="normal"
@@ -65,6 +88,7 @@ const LogIn = () => {
             name="email"
             autoComplete="email"
             autoFocus
+            onChange={()=>setEmail(event.target.value)}
           />
           <TextField
             variant="outlined"
@@ -76,33 +100,21 @@ const LogIn = () => {
             type="password"
             id="password"
             autoComplete="current-password"
+            onChange={()=>setPassword(event.target.value)}
           />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
+          <Typography component="h1" variant="subtitle2" color="error">
+            {errorMsg}
+          </Typography>  
           <Button
             type="submit"
             fullWidth
             variant="contained"
             color="primary"
             className={classes.submit}
+            onClick={() => login(email, password)}
           >
             Sign In
           </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
       </div>
       <Box mt={8}>
         <Footer />
