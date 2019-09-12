7f069c17849ec6a0b79ab462f90be3870cdbbcbb
Miguel Brandão <mickael.a.brandao@gmail.com>
2019-09-12 20:24:32 +0100

    Add sign-up page

--- components/NavBar.js
+++ components/NavBar.js
@@ -5,6 +5,7 @@ import Box from '@material-ui/core/Box';
 import Toolbar from '@material-ui/core/Toolbar';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import Link from '../src/Link';
 
 const StyledTypography = styled(Typography)({
   color: 'black',
@@ -27,10 +28,15 @@ const Entry = ({ name }) => (
   </StyledTypography>
   );
 
+// The signup link is not intended to stay in the navbar;
+// it's there for development only. It may be incorporated into 
+// the signin page in the future if deemed apropriate
 const NavBar = () => (
   <StyledBox>
     <StyledToolBar>
-      <Entry name="Test1" />
+      <Link href="/">
+        <Entry name="Index" />
+      </Link>
       <Grid
           container
           direction="row-reverse"
@@ -38,6 +44,11 @@ const NavBar = () => (
           alignItems="center"
           spacing={1}
       >
+        <Grid item>
+          <Link href="/signup">
+            <Entry name="Signup" />
+          </Link>
+        </Grid>
         <Grid item>
           <Entry name="Test2" />
         </Grid>
---
+++ pages/signup.js
@@ -0,0 +1,211 @@
+import React from 'react';
+import { Avatar, Button, CssBaseline, TextField, Grid, Typography, Container } from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { makeStyles } from '@material-ui/core/styles';
+import fetch from 'isomorphic-unfetch';
+import { useRouter } from 'next/router';
+import Link from '../src/Link';
+import Layout from '../components/Layout';
+
+const useStyles = makeStyles(theme => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white,
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+SignUp.getInitialProps = ({query}) => {
+  return {query}
+}
+
+export default function SignUp() {
+  const classes = useStyles();
+  
+  const router = useRouter()
+  let UUID = router.query.id;
+  //console.log(UUID);
+
+  const [errorMsg, setErrorMsg] = React.useState('');
+
+  const useSignUpForm = (callback) => {
+    const [attendee, setAttendee] = React.useState({
+      id: UUID,
+      nickname: ''
+    });
+    const [inputs, setInputs] = React.useState({
+      email: '',
+      password: '',
+      password_confirmation: '',
+      attendee: ''
+    });
+    const handleSubmit = (event) => {
+      if (event) {
+        event.preventDefault();
+      }
+      callback();
+    }
+    const handleInputChange = (event) => {
+      event.persist();
+      setInputs(inputs => ({...inputs, [event.target.name]: event.target.value}));
+    }
+    const handleNicknameChange = (event) => {
+      event.persist();
+      setAttendee(attendee => ({...attendee, [event.target.name]: event.target.value}));
+    }
+    return {
+      handleSubmit,
+      handleInputChange,
+      handleNicknameChange,
+      inputs,
+      attendee
+    };
+  }
+
+
+  const signup = () => {
+
+    //TODO: add warning page if there is already an active session
+    const signupData = {user: {...inputs, attendee: attendee}};
+    //console.log(signupData);
+    const apiEndpoint = process.env.REACT_APP_ENDPOINT + process.env.REACT_APP_API_AUTH_SIGN_UP;
+    fetch(apiEndpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(signupData)
+    })
+    .then((res) => res.json())
+    .then((res) => {
+        if(res.jwt){
+          setErrorMsg('');
+          localStorage.jwt = res.jwt;
+          window.location.pathname = "/";
+        }
+        else if (res.error){ 
+          setErrorMsg(res.error);
+        }
+        else if (res.errors.detail){ 
+          setErrorMsg(res.errors.detail);
+        }
+    });
+  }
+
+  const {inputs, attendee, handleInputChange, handleNicknameChange, handleSubmit} = useSignUpForm(signup);
+
+  return (
+    <Layout>
+    <Container component="main" maxWidth="xs">
+    <CssBaseline />
+    <div className={classes.paper}>
+      <Avatar className={classes.avatar}>
+      <LockOutlinedIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+      Sign up
+      </Typography>
+      <form className={classes.form} noValidate onSubmit={handleSubmit}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+        <TextField
+          id="nickname"
+          name="nickname"
+          label="Nickname"
+          autoComplete="nickname"
+          onChange={handleNicknameChange}
+          value={attendee.nickname}
+          variant="outlined"
+          required
+          fullWidth
+          autoFocus
+        />
+        </Grid>
+        <Grid item xs={12}>
+        <TextField
+          id="email"
+          name="email"
+          label="Email Address"
+          autoComplete="email"
+          onChange={handleInputChange}
+          value={inputs.email}
+          variant="outlined"
+          required
+          fullWidth
+        />
+        </Grid>
+        <Grid item xs={12}>
+        <TextField
+          id="password"
+          name="password"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          onChange={handleInputChange}
+          value={inputs.password}
+          variant="outlined"
+          required
+          fullWidth
+        />
+        </Grid>
+        <Grid item xs={12}>
+        <TextField
+          id="password_confirmation"
+          name="password_confirmation"
+          label="Re-enter password"
+          type="password"
+          autoComplete="current-password"
+          onChange={handleInputChange}
+          value={inputs.password_confirmation}
+          variant="outlined"
+          required
+          fullWidth
+        />
+        </Grid>
+      </Grid>
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        color="primary"
+        className={classes.submit}
+      >
+        Sign Up
+      </Button>
+      <Grid container direction="column" justify="center" alignItems="center">
+        <Grid item>
+        <Typography component="h1" variant="subtitle2" color="error">
+          {errorMsg}
+        </Typography>
+        </Grid>
+        <Grid item>
+        {// TODO: add signin page link}
+        }
+        <Link href="/" variant="body2">
+          Already have an account? Sign in
+        </Link>
+        </Grid>
+      </Grid>
+      </form>
+    </div>
+    </Container>
+    </Layout>
+  );
+}
