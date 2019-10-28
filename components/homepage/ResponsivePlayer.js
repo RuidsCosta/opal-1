fa737603554ee79a6bdb8a8bd16629c2face4e42
Luís Alves <luismig.alves@gmail.com>
2019-10-28 14:44:19 +0000

    Fixed identation

--- components/homepage/ResponsivePlayer.js
+++ components/homepage/ResponsivePlayer.js
@@ -5,34 +5,34 @@ import PropTypes from 'prop-types';
 import ReactPlayer from 'react-player';
 
 const CustomPlayer = styled(ReactPlayer)({
-	position: 'absolute',
-	height: '100%',
-	width: '100%',
-	boxShadow: '1em -1em 0.5em #BEBEBE',
-	top: 0,
-	left: 0,
+  position: 'absolute',
+  height: '100%',
+  width: '100%',
+  boxShadow: '1em -1em 0.5em #BEBEBE',
+  top: 0,
+  left: 0,
 });
 
 const Wrapper = styled(Box)({
-	position: 'relative',
-	paddingTop: '56.25%',
+  position: 'relative',
+  paddingTop: '56.25%',
 });
 
 function ResponsivePlayer({ url }) {
-    return (
-		<Wrapper>
-			<CustomPlayer
-				url={url}
-				playing
-				width="100%"
-				height="100%"
-			/>
-		</Wrapper>
-    );
+  return (
+    <Wrapper>
+      <CustomPlayer
+        url={url}
+        playing
+        width="100%"
+        height="100%"
+      />
+    </Wrapper>
+  );
 }
 
 ResponsivePlayer.propTypes = {
-    url: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
 };
 
 export default ResponsivePlayer;
